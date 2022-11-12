d5e00ea03d8d4f184596f0be174a6ce21de05ca9
Manuch Sadri <colonquebang@hotmail.com>
2022-11-12 11:20:20 -0800

    removed unneeded files, and updated App.js for basic Routes layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import Main from './Main';
+import Profile from './Profile';
+import About from './About';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <Router>
+
+          <Header />
+
+          <Routes>
+
+            <Route
+              path='/'
+              element={<Main />}
+            ></Route>
+
+            <Route
+              path="/profile"
+              element={<Profile />}
+            ></Route>
+
+            <Route
+              path='/about'
+              element={<About />}
+            ></Route>
+
+          </Routes>
+
+          <Footer />
+          
+        </Router>
+      </>
+    )
+  }
 }
 
 export default App;
